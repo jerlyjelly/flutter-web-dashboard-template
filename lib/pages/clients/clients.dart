f45b35b3d2ca67e52f43ec7978a4b3630d8c9fe9
jerlyjelly <68527525+jerlyjelly@users.noreply.github.com>
2021-08-13 22:58:58 +0900

    updated teams table

--- lib/pages/clients/clients.dart
+++ lib/pages/clients/clients.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web_dashboard/constants/controllers.dart';
+import 'package:flutter_web_dashboard/constants/style.dart';
 import 'package:flutter_web_dashboard/helpers/reponsiveness.dart';
 import 'package:flutter_web_dashboard/pages/clients/widgets/clients_table.dart';
 import 'package:flutter_web_dashboard/widgets/custom_text.dart';
 import 'package:get/get.dart';
 
-class ClientsPage extends StatelessWidget {
+class ClientsPage extends StatefulWidget {
   const ClientsPage({Key key}) : super(key: key);
 
+  @override
+  _ClientsPageState createState() => _ClientsPageState();
+}
+
+class _ClientsPageState extends State<ClientsPage> {
+  String _dropDownValue;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -26,12 +33,57 @@ class ClientsPage extends StatelessWidget {
               ],
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(15, 20, 15, 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                DropdownButton(
+                  hint: _dropDownValue == null
+                      ? CustomText(text: 'Team 1')
+                      : CustomText(
+                          text: _dropDownValue,
+                          size: 30,
+                        ),
+                  isExpanded: false,
+                  underline: SizedBox(),
+                  iconSize: 30.0,
+                  items: [
+                    'Team 1',
+                    'Team 2',
+                    'Team 3',
+                  ].map(
+                    (val) {
+                      return DropdownMenuItem<String>(
+                        value: val,
+                        child: Text(val),
+                      );
+                    },
+                  ).toList(),
+                  onChanged: (val) {
+                    setState(
+                      () {
+                        _dropDownValue = val;
+                      },
+                    );
+                  },
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    // Respond to button press
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: active,
+                  ),
+                  icon: Icon(Icons.add, size: 18),
+                  label: Text("Add Team"),
+                ),
+              ],
+            ),
+          ),
           Expanded(
-              child: ListView(
-            children: [
-              Clientstable(),
-            ],
-          )),
+            child: Clientstable(),
+          ),
         ],
       ),
     );
