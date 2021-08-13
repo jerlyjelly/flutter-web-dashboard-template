2d14d6e07620e34be2e7cb155605e04102d97003
jerlyjelly <68527525+jerlyjelly@users.noreply.github.com>
2021-08-13 23:45:20 +0900

    add student button

--- lib/pages/clients/clients.dart
+++ lib/pages/clients/clients.dart
@@ -17,74 +17,81 @@ class _ClientsPageState extends State<ClientsPage> {
   String _dropDownValue;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Obx(
-            () => Row(
-              children: [
-                Container(
-                    margin: EdgeInsets.only(top: ResponsiveWidget.isSmallScreen(context) ? 56 : 6),
-                    child: CustomText(
-                      text: menuController.activeItem.value,
-                      size: 24,
-                      weight: FontWeight.bold,
-                    )),
-              ],
+    return Scaffold(
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: Text('Add Student'),
+        icon: Icon(Icons.add),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Obx(
+              () => Row(
+                children: [
+                  Container(
+                      margin: EdgeInsets.only(top: ResponsiveWidget.isSmallScreen(context) ? 56 : 6),
+                      child: CustomText(
+                        text: menuController.activeItem.value,
+                        size: 24,
+                        weight: FontWeight.bold,
+                      )),
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(15, 20, 15, 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                DropdownButton(
-                  hint: _dropDownValue == null
-                      ? CustomText(text: 'Team 1')
-                      : CustomText(
-                          text: _dropDownValue,
-                          size: 30,
-                        ),
-                  isExpanded: false,
-                  underline: SizedBox(),
-                  iconSize: 30.0,
-                  items: [
-                    'Team 1',
-                    'Team 2',
-                    'Team 3',
-                  ].map(
-                    (val) {
-                      return DropdownMenuItem<String>(
-                        value: val,
-                        child: Text(val),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 20, 15, 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  DropdownButton(
+                    hint: _dropDownValue == null
+                        ? CustomText(text: 'Team 1')
+                        : CustomText(
+                            text: _dropDownValue,
+                            size: 30,
+                          ),
+                    isExpanded: false,
+                    underline: SizedBox(),
+                    iconSize: 30.0,
+                    items: [
+                      'Team 1',
+                      'Team 2',
+                      'Team 3',
+                    ].map(
+                      (val) {
+                        return DropdownMenuItem<String>(
+                          value: val,
+                          child: Text(val),
+                        );
+                      },
+                    ).toList(),
+                    onChanged: (val) {
+                      setState(
+                        () {
+                          _dropDownValue = val;
+                        },
                       );
                     },
-                  ).toList(),
-                  onChanged: (val) {
-                    setState(
-                      () {
-                        _dropDownValue = val;
-                      },
-                    );
-                  },
-                ),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    // Respond to button press
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: active,
                   ),
-                  icon: Icon(Icons.add, size: 18),
-                  label: Text("Add Team"),
-                ),
-              ],
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      // Respond to button press
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: active,
+                    ),
+                    icon: Icon(Icons.add, size: 18),
+                    label: Text("Add Team"),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: Clientstable(),
-          ),
-        ],
+            Expanded(
+              child: Clientstable(),
+            ),
+          ],
+        ),
       ),
     );
   }
